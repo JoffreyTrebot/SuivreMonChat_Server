f8f0bf4aa8a3833e024ab7e4c8d9f8e4ad6eb7a8
Joffrey Trébot <trebotjoffrey@gmail.com>
2017-12-26 23:57:03 +0100

    Users controller
    - Restriction for add user and search user

--- routes/usersCtrl.js
+++ routes/usersCtrl.js
@@ -1,11 +1,54 @@
+import { userInfo } from 'os';
+
 // Imports
 var bcrypt = require('bcrypt');
 var jwt = require('jsonwebtoken');
+var models = require('../models');
 
 //Routes
 module.exports = {
     register: function(req, res) {
-        //TODO: To implement
+
+        //Params
+        var email = req.body.email;
+        var username = req.body.username;
+        var password = req.body.password;
+        var numgps = req.body.numgps;
+
+        if (email == null || username == null || password == null){
+            return res.status(400).json({ 'error': ' missing parameters'});
+        }
+
+        // TODO verify pseudo lenght, mail regex, password etc.
+        models.User.findOne({
+            attributes: ['email'],
+            where: { email: email}
+        })
+        .then(function(userFound){
+            if(!userFound){
+                bcrypt.hash(password, 5, function( err, bcryptedPassword){
+                    var newUser = models.User.create({
+                        email: email,
+                        username: username,
+                        password: password,
+                        numgps: numgps
+                    })
+                })
+                .then(function(newUser){
+                    return res.status(201).json({
+                        'userId': newUser.id
+                    })
+                })
+                .catch(function(err){
+                    return res.status(500).json({'error': 'cannot add user'});
+                });
+            } else {
+                return res.status(409).json({ 'error': ' user already exist'});
+            }
+        })
+        .catch(function(err){
+            return res.status(500).json({'error': 'unable to verify user'});
+        });
     },
     login: function(req, res){
         //TODO: To implement
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Imports
 var express = require('express');
 var bodyParser = require('body-parser');
+var apiRouter = require('./apiRouter').router;
 
 // Intantiate server
 var server = express();
@@ -14,6 +15,8 @@ server.get('/', function(req, res){
     res.setHeader('Content-Type', 'text/html');
     res.status(200).send('<h1>Bonjour sur mon serveur !</h1>');
 });
+
+server.use('/api/', apiRouter);
 
 // Launch server
 server.listen(8081, function(){
