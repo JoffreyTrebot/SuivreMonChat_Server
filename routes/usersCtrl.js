a77d027c13d666ddab0197d4a438d486f4507289
Joffrey Trébot <trebotjoffrey@gmail.com>
2017-12-26 23:28:52 +0100

    Controller users

---
+++ routes/usersCtrl.js
@@ -0,0 +1,13 @@
+// Imports
+var bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+
+//Routes
+module.exports = {
+    register: function(req, res) {
+        //TODO: To implement
+    },
+    login: function(req, res){
+        //TODO: To implement
+    }
+}
