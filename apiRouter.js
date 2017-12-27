6b49f3360c76e5326147ecd10aef0513768c5dc7
Joffrey Trébot <trebotjoffrey@gmail.com>
2017-12-27 21:14:38 +0100

    update and correctly bugs

--- apiRouter.js
+++ apiRouter.js
@@ -1,14 +1,14 @@
 // Imports
-var express = require('express');
+var express   = require('express');
 var usersCtrl = require('./routes/usersCtrl');
 
 // Router
-exports.router = (function(){
-    var apiRouter = express.Router();
+exports.router = (function() {
+  var apiRouter = express.Router();
 
-    // Users routes
-    apiRouter.route('/users/register/').post(usersCtrl.register);
-    apiRouter.route('/users/login/').post(usersCtrl.login);
+  // Users routes
+  apiRouter.route('/users/register/').post(usersCtrl.register);
+  apiRouter.route('/users/login/').post(usersCtrl.login);
 
-    return apiRouter;
+  return apiRouter;
 })();
--- routes/usersCtrl.js
+++ routes/usersCtrl.js
@@ -1,54 +1,92 @@
 // Imports
-var bcrypt = require('bcrypt');
-var jwt = require('jsonwebtoken');
-var models = require('../models');
+var bcrypt    = require('bcrypt');
+var jwtUtils  = require('../utils/jwt.utils');
+var models    = require('../models');
 
-//Routes
+// Routes
 module.exports = {
-    register: function(req, res) {
+  register: function(req, res) {
+    
+    // Params
+    var email    = req.body.email;
+    var username = req.body.username;
+    var password = req.body.password;
+    var numgps = req.body.numgps;
 
-        //Params
-        var email = req.body.email;
-        var username = req.body.username;
-        var password = req.body.password;
-        var numgps = req.body.numgps;
-
-        if (email == null || username == null || password == null || numgps == null){
-            return res.status(400).json({ 'error': ' missing parameters'});
-        }
-
-        // TODO verify pseudo lenght, mail regex, password etc.
-        models.User.findOne({
-            attributes: ['email'],
-            where: { email: email}
-        })
-        .then(function(userFound){
-            if(!userFound){
-                bcrypt.hash(password, 5, function( err, bcryptedPassword){
-                    var newUser = models.User.create({
-                        email: email,
-                        username: username,
-                        password: password,
-                        numgps: numgps
-                    })
-                })
-                .then(function(newUser){
-                    return res.status(201).json({
-                        'userId': newUser.id
-                    })
-                })
-                .catch(function(err){
-                    return res.status(500).json({'error': 'cannot add user'});
-                });
-            } else {
-                return res.status(409).json({ 'error': ' user already exist'});
-            }
-        })
-        .catch(function(err){
-            return res.status(500).json({'error': 'unable to verify user'});
-        });
-    },
-    login: function(req, res){
-        //TODO: To implement
+    if (email == null || username == null || password == null || numgps == null) {
+      return res.status(400).json({ 'error': 'missing parameters' });
     }
+
+    // TODO verify pseudo length, mail regex, password etc.
+
+    models.User.findOne({
+      attributes: ['email'],
+      where: { email: email }
+    })
+    .then(function(userFound) {
+      if (!userFound) {
+
+        bcrypt.hash(password, 5, function( err, bcryptedPassword ) {
+          var newUser = models.User.create({
+            email: email,
+            username: username,
+            password: bcryptedPassword,
+            numgps: numgps
+          })
+          .then(function(newUser) {
+            return res.status(201).json({
+              'userId': newUser.id
+            })
+          })
+          .catch(function(err) {
+            return res.status(500).json({ 'error': 'cannot add user' });
+          });
+        });
+
+      } else {
+        return res.status(409).json({ 'error': 'user already exist' });
+      }
+    })
+    .catch(function(err) {
+      return res.status(500).json({ 'error': 'unable to verify user' });
+    });
+
+  },
+  login: function(req, res) {
+    
+    // Params
+    var email    = req.body.email;
+    var password = req.body.password;
+
+    if (email == null ||  password == null) {
+      return res.status(400).json({ 'error': 'missing parameters' });
+    }
+
+    // TODO verify mail regex & password length.
+
+    models.User.findOne({
+      where: { email: email }
+    })
+    .then(function(userFound) {
+      if (userFound) {
+        
+        bcrypt.compare(password, userFound.password, function(errBycrypt, resBycrypt) {
+          if(resBycrypt) {
+            return res.status(200).json({
+              'userId': userFound.id,
+              'token': jwtUtils.generateTokenForUser(userFound)
+            });
+          } else {
+            return res.status(403).json({ 'error': 'invalid password' });
+          }
+        });
+
+      } else {
+        return res.status(404).json({ 'error': 'user not exist in DB' });
+      }
+    })
+    .catch(function(err) {
+      return res.status(500).json({ 'error': 'unable to verify user' });
+    });
+  }
 }
